d258da3097e9dcaa534177de2f142f582907c4e2
Gustavo Victor <gguvictor909@gmail.com>
2023-06-24 21:07:37 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div className='app'>
+      <h1>Ola</h1>
+      <hr />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium obcaecati doloribus libero ipsum eum adipisci distinctio odit error porro voluptatum, saepe, ullam, labore unde. Eligendi dignissimos incidunt quam asperiores non.</p>
+    </div>
+  )
+}
+
+export default App; 
